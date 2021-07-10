4992eab7936b56bb79a1b68bd7ef90271aa8dc38
stivv <stevestivv@gmail.com>
2021-07-10 18:22:40 +0300

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,13 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/recipe/:id",
+    name: "recipe",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (recipe.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "recipe" */ "../views/Recipe.vue"),
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <h1>Recipes</h1>
+  <ul>
+    <li v-for="recipe in recipes" :key="recipe.id">
+      <router-link :to="{ path: `/recipe/${recipe.id}` }">
+        {{ recipe.name }}
+      </router-link>
+    </li>
+  </ul>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+const recipes = [
+  { id: 1, name: "Recipe 1", description: "this is the description" },
+  { id: 2, name: "Recipe 2", description: "this is the description" },
+  { id: 3, name: "Recipe 3", description: "this is the description" },
+  { id: 4, name: "Recipe 4", description: "this is the description" },
+];
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+ul li {
+  padding: 10px 0;
+}
+</style>
